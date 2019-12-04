011eb578797a788324bce5df0e90d7c89399398b
MCSMLab <nathan@mcsmlab.com>
2019-12-04 15:57:21 -0800

    Update Get-Delegate.ps1

--- Get-Delegate.ps1
+++ Get-Delegate.ps1
@@ -1,35 +1,39 @@
-
 <#
 .SYNOPSIS
-    This script lists delegates for all mailbox folders.
+This script lists delegates for all mailbox folders.
 
 .DESCRIPTION
-    This script lists delegates for all mailbox folders. Works on Exchange 2010+
+This script lists delegates for all mailbox folders. Works on Exchange 2010+
 
 .NOTES
-    Get-Delegate
-    v1.1
-    12/2/2019
-    By Nathan O'Bryan
-    nathan@mcsmlab.com
-    http://www.mcsmlab.com
+Get-Delegate
+v1.5
+12/4/2019
+By Nathan O'Bryan
+nathan@mcsmlab.com
+http://www.mcsmlab.com
 
-    Change Log
-    1.0 -
-    1.1 - Added status bar
+Change Log
+1.0 -
+1.1 - Added status bar
+1.5 - Added auto detection for EMS. If script isn't run in EMS it will ask you to enter FQDN of Exchange server for remote PS connection
 
 .LINK
-    https://github.com/MCSMLab/Get-Delegate/blob/master/Get-Delegate.ps1
+https://github.com/MCSMLab/Get-Delegate/blob/master/Get-Delegate.ps1
 
 .EXAMPLE
-    .\Get-Delegates.ps1
+.\Get-Delegates.ps1
 #>
 
 Clear-Host
-$Answer = Read-Host "Do you want to connect to a remote Exchange server? [Y/N]"
 
-If ($Answer -eq "Y" -or $Answer -eq "y")
+If ($exscripts)
 {
+    Write-Host 'Exchange Management Shell loaded'
+}
+
+Else
+{   
     $ExchangeServer = Read-Host "Enter the FQDN of your Exchange server"
     $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Authentication Kerberos
     Import-PSSession $Session
@@ -45,12 +49,13 @@ ForEach ($UserMailbox in $UserMailboxes)
     $Mailbox = "" + $UserMailbox.PrimarySmtpAddress
     $MailboxName = "" + $UserMailbox.Name
     $Folders = Get-MailboxFolderStatistics $Mailbox | ForEach-Object {$_.FolderPath} | ForEach-Object {$_.Replace(“/”,”\”)}
-
+    $MailboxName >> .\DelegateReport.txt
+    
     ForEach ($Folder in $Folders)
     {
         $FolderPath = $Mailbox + ":" + $Folder
         $Permissions = Get-MailboxFolderPermission -Identity $FolderPath -ErrorAction SilentlyContinue
         $Permissions = $Permissions | Where-Object { ($_.User -NotLike "Default") -And ($_.User -NotLike "Anonymous") -And ($_.AccessRights -NotLike "None") -And ($_.AccessRights -NotLike "Owner") }
-        $Permissions | Select-Object $MailboxName, User, FolderName, AccessRights | Export-Csv -Path .\DelegateReport.csv
+        $Permissions | Select-Object User, FolderName, AccessRights | Format-Table  >> .\DelegateReport.txt
     }
 }
